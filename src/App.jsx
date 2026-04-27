ee2efd3bcea1787205c7362fd31d4a4659fb9d9d
diiiana23 <diana.alexandrescu23@gmail.com>
2026-04-27 17:56:18 +0300

    feat: add HomePage landing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import HomePage from "./pages/HomePage";
+
+<Route path="/" element={<HomePage />} />
 
 function App() {
   const [count, setCount] = useState(0)
