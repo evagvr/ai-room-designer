cf01add299c4268f30310fefee5e7d4de77894bf
diiiana23 <diana.alexandrescu23@gmail.com>
2026-04-27 17:34:15 +0300

    feat: add route for ComparePage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import ComparePage from "./pages/ComparePage";
+
+<Route path="/compare" element={<ComparePage />} />
 
 function App() {
   const [count, setCount] = useState(0)
